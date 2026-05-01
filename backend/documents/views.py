919cd0ed23d92fd15937a1e90b6c667116ff5dc3
Chris Hembree <chembree08@gmail.com>
2026-04-30 19:42:26 -0500

    working on document uploader

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -8,23 +8,33 @@ from .models import Document
 from .serializers import DocumentSerializer
 
 
+
+
+
 def test_view(request):
     return JsonResponse({
         "message": "Hello from Django!"
     })
 
 
-@api_view(['POST'])
+
+@api_view(['GET', 'POST'])
 def upload_document(request):
-    serializer = DocumentSerializer(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == 'GET':
+        documents = Document.objects.all()
+        serializer = DocumentSerializer(documents, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST':
+        serializer = DocumentSerializer(data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 
--- backend/docunow_backend/settings.py
+++ backend/docunow_backend/settings.py
@@ -136,6 +136,22 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
