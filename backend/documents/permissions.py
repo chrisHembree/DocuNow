4dd44df243d65375fee98732f11f4f09c5ee3b14
Chris Hembree <chembree08@gmail.com>
2026-06-11 00:01:31 -0500

    permissions

---
+++ backend/documents/permissions.py
@@ -0,0 +1,76 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            name='Admins'
+        ).exists()
+
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            name='Managers'
+        ).exists()
+
+
+class IsEmployee(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            name='Employees'
+        ).exists()
+
+
+class IsViewer(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            name='Viewers'
+        ).exists()
+    
+
+class CanViewDocuments(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.groups.filter(
+                name__in=[
+                    'Admins',
+                    'Managers',
+                    'Employees',
+                    'Viewers',
+                ]
+            ).exists()
+        )
+
+
+class CanUploadDocuments(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.groups.filter(
+                name__in=[
+                    'Admins',
+                    'Managers',
+                    'Employees',
+                ]
+            ).exists()
+        )
+
+
+class CanModifyDocuments(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.groups.filter(
+                name__in=[
+                    'Admins',
+                    'Managers',
+                ]
+            ).exists()
+        )
+
+
+
+
+
+
+
+
